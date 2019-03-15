8bfac122e67f7b7b0e042c051863d72ab235f47e
Velislav Velinov <vevelinov@hotels.com>
2019-03-15 17:05:25 +0000

    Add categories

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
 from profanity_check import predict
+from nltk.stem import PorterStemmer
 
 app = Flask(__name__)
 
@@ -13,14 +14,22 @@ def classifyTweet():
     json = request.get_json()
     tweet_responses = []
     for tweet in json['tweets']:
-        tweet_responses.append(processTweet(tweet))
+        tweet_responses.append(processTweet(tweet, json['categories']))
     response = {'tweets': tweet_responses}
     return jsonify(response)
 
-def processTweet(tweet):
-    polarity = TextBlob(tweet['text']).sentiment.polarity
+def processTweet(tweet, categories):
+    porter = PorterStemmer()
+
+    categories_stemmed = list(map(lambda cat: porter.stem(cat), categories))
+    tweet_stems = list(map(lambda cat: porter.stem(cat), tweet['text'].split()))
+
+    contains_prohibited_categories = any(elem in tweet_stems for elem in categories_stemmed)
+    is_polarising = TextBlob(tweet['text']).sentiment.polarity < -0.35
     is_profane = 1 in predict([tweet['text']])
-    offensive_filter_value = polarity < -0.35 or is_profane
+
+    offensive_filter_value = is_polarising or is_profane or contains_prohibited_categories
+
     response = {'id': tweet['id'], 'filter': offensive_filter_value}
     return response
 
